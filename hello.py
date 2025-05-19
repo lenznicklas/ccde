181beec85acd37b416ccfc66d685db4c37d31b42
lenznicklas <lnicklas43@gmail.com>
2025-05-19 10:48:18 +0200

    c1

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Eingabe vom Benutzer
+zahl = int(input("Gib eine ganze Zahl ein: "))
+
+# Überprüfung auf gerade oder ungerade
+if zahl % 2 == 0:
+    print("Die Zahl ist gerade.")
+else:
+    print("Die Zahl ist ungerade.")
